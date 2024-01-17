c9c0a82f808f64a1b1207d0244b8ff786a8f3d49
Petar Petrov <115663030+PetarAtanasov@users.noreply.github.com>
2024-01-17 11:28:00 +0100

    Update codeSecondPage.js

--- codeSecondPage.js
+++ codeSecondPage.js
@@ -1,11 +1,15 @@
-const urlParams = new URLSearchParams(window.location.search);
-const validatedUsername = urlParams.get('username');
-const usernameDisplay = document.getElementById('usernameDisplay');
+document.addEventListener("DOMContentLoaded", function () {
+  const urlParams = new URLSearchParams(window.location.search);
+  const emailParam = urlParams.get("email");
+  const usernameDisplay = document.getElementById("usernameDisplay");
 
-if (validatedUsername) {
-    const names = validatedUsername.split(' ');
-    const formattedName = names.map(name => name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()).join(' ');
+  if (emailParam) {
+    const names = emailParam.split("@")[0].split("."); // Extract username from email
+    const formattedName = names
+      .map((name) => name.charAt(0).toUpperCase() + name.slice(1).toLowerCase())
+      .join(" ");
     usernameDisplay.textContent = formattedName;
-} else {
-    usernameDisplay.textContent = 'Guest';
-}
+  } else {
+    usernameDisplay.textContent = "Guest";
+  }
+});
